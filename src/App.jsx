b74d575f4673ca2692ee93df0f56c0907f6e3d18
Oldrado Junior <oldradojunior@gmail.com>
2023-09-14 02:00:17 -0300

    feat: add history for the router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyles';
 import Header from './components/Header';
 import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <Header />
       <Routes />
       <GlobalStyle />
-    </BrowserRouter>
+    </Router>
   );
 }
 
