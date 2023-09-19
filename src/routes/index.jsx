9d07f23ce92d6e47bd9fa5896341dfdd8514dbaf
Oldrado Junior <oldradojunior@gmail.com>
2023-09-19 01:49:26 -0300

    fix: open login route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -7,7 +7,7 @@ import MyRoute from './MyRoute';
 function Routes() {
   return (
     <Switch>
-      <MyRoute exact path="/" component={Login} isClosed />
+      <MyRoute exact path="/" component={Login} />
       <MyRoute path="*" component={Page404} />
     </Switch>
   );
